c7afacb47c12e5cc8494042cdea835ebdc44c8b2
Zero-zx <dthieu2512@gmail.com>
2023-05-21 17:40:11 +0700

    [EDIT] make class Dog implement Predator interface, add override method for action value

--- Example/src/main/kotlin/Dog.kt
+++ Example/src/main/kotlin/Dog.kt
@@ -1,10 +1,11 @@
-class Dog(val name: String) : Animal(name){
+class Dog(val name: String) : Animal(name), Predator{
     override val legs: Int = 4
     override val type: String
         get() = "dog"
     override fun showName() {
         println("This is a $type named: $name")
     }
-    //override fun showMessage() {}
-    // error because showMessage is declared with "final"
+
+    override val action: String = "Serching for cat"
+    // Have to override action value because it hasn't been defined yet.
 }
