3563bd2cccc4f5e7187bc1018d81d6f440456b76
Zero-zx <dthieu2512@gmail.com>
2023-05-21 18:03:22 +0700

    [EDIT] add instance for Mouse class in Main, test some methods

--- Example/src/main/kotlin/Main.kt
+++ Example/src/main/kotlin/Main.kt
@@ -2,16 +2,28 @@
 
 fun main() {
     val cat = Cat("Tom")
+    val mouse = Mouse("Jerry")
     val dog = Dog("Spike")
 
     println("\n---After initializing---\n")
 
     cat.showName()
     cat.isMove()
+    cat.isHunting(cat.name)
+    cat.isRunning(cat.name)
+    println(cat.action) // action in this line has been overrided
+
+    println("---")
+
+    mouse.showName()
+    mouse.isMove()
+    mouse.isRunning(mouse.name)
+    println(mouse.action) // action in this line is from interface.
 
     println("---")
 
     dog.showName()
     dog.isMove()
-
+    dog.isHunting(dog.name)
+    println(dog.action) // action in this line has been overrided
 }
