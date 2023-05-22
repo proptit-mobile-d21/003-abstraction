527b361d43b6608922a6b7a6a325d5d768212cd5
mdunggggg <dungmanh74@gmail.com>
2023-05-23 01:23:07 +0700

    Explain the difference between Abstract class  and Interface

--- Abtract/src/main/kotlin/Main.kt
+++ Abtract/src/main/kotlin/Main.kt
@@ -39,5 +39,15 @@ fun main(){
     val plane = Plane()
     plane.fly()
 
+    /*
+        - Class Bird và Dog đều kế thừa từ class Animal, nhưng class Bird có thể implement interface IFlyable
+        còn class Dog thì không thể. Điều này là do Bird có thể bay, còn Dog thì không thể bay.
+        - Như vậy abstract class có thể coi là cha cho tất cả các class con có cùng bản chất với nó.
+        - Còn Interface là một chức năng có thể thêm vào bất kỳ vào class nào
+        - 2 class Plane và Bird có bản chất hoàn toàn khác nhau nhưng mà đều có thể bay, vì vậy ta có thể implement
+        interface IFlyable cho cả 2 class này.
+        - Nói ngắn gọn,  quan hệ giữa một class khi thừa kế một abstract class được gọi là is-a,
+        và một class khi hiện thực một interface được gọi là can-do
+     */
 
 }
