e6c75d8a35e64bb94f803469a44698b5eaf2a1f4
mdunggggg <dungmanh74@gmail.com>
2023-05-23 01:18:28 +0700

    Create class Bá»ird áºáºextends Animal, implements ÃIFlyable and class Plane implements ÃIFlyable

--- Abtract/src/main/kotlin/Main.kt
+++ Abtract/src/main/kotlin/Main.kt
@@ -13,9 +13,31 @@ class Dog: Animal(){
 interface IFlyable{
     fun fly()
 }
+class Bird: Animal(), IFlyable {
+    override fun makeSound() {
+        println("Chirp chirp")
+    }
 
+    override fun fly() {
+        println("I am flying")
+    }
+}
+class Plane: IFlyable{
+    override fun fly() {
+        println("Plane is flying")
+    }
+}
 fun main(){
     val dog = Dog()
     dog.makeSound()
     dog.eat()
+
+    val bird = Bird()
+    bird.makeSound()
+    bird.fly()
+
+    val plane = Plane()
+    plane.fly()
+
+
 }
