7867e0a46b600620a8c67ef24498c5ac00d016a2
NhuQuynhLea <quynhln309@gmail.com>
2023-05-25 02:27:10 +0700

    add vd abstract, interface và phân biệt

--- Main.kt
+++ Main.kt
@@ -1,20 +1,58 @@
-//Interface: - abstract member, abstract const, fun but not implementation
-// - cannot have init block or constructor
-//abstract class: same interface but have constructor and init
-// - extend abstract -> need constructor ()
-// can only inherit a single class => solve this we use Interface
-// Interface: all func are abstract in default
 
-abstract  class test {
-    init {
+abstract class Animal(){
+    abstract fun sound()
+}
+interface RunAble{
+    fun Run()
+}
+interface SwimAble{
+    fun swim()
+}
+class Dog : Animal(),RunAble{ // dog thuộc lớp động vật và kế thừa những đặc điểm của động vật và có thể chạy
+    override fun sound() {
+        println("gau gau")
+    }
 
+    override fun Run() {
+        println("can run")
     }
 }
+class Duck: Animal(),SwimAble{ // vịt thuộc lớp động vật và kế thừa những đặc điểm của động vật và có thể bơi
+    override fun sound() {
+        println("Quac quac")
+    }
 
-fun main(args: Array<String>) {
-    println("Hello World!")
+    override fun swim() {
+        println("can swim")
+    }
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
 }
+class Cat : Animal(),RunAble{ // mèo thuộc lớp động vật và kế thừa những đặc điểm của động vật và có thể chạy
+    override fun sound() {
+        println("meow meow")
+    }
+
+    override fun Run() {
+        println("can run")
+    }
+}
+fun main(args: Array<String>) {
+    val dog = Dog()
+    dog.sound()
+    dog.Run()
+    val duck = Duck()
+    duck.sound()
+    duck.swim()
+    val cat = Cat()
+    cat.sound()
+    cat.Run()
+
+}
+
+//Abstract classes should be used primarily for objects that are closely related, whereas interfaces are best suited for
+// providing a common functionality to unrelated classes
+
+
+//Cần sử dụng tính abstract:
+//- Để đảm bảo tính bảo mật và chỉ hiển thị những thông tin cần thiết cho người sử dụng
+//- Thống nhất các class có đặc điểm giống nhau => code ngắn gọn, đơn giản, tái sử dụng code
