8d288c4e94a53647b1459e541ed5168fb4963d90
mdunggggg <dungmanh74@gmail.com>
2023-05-26 16:22:56 +0700

    Create Sealed class Shape

--- Sealed_Class/src/main/kotlin/Main.kt
+++ Sealed_Class/src/main/kotlin/Main.kt
@@ -1,4 +1,8 @@
-
+sealed class Shape{
+    object None : Shape()
+    data class Rectangle(val width: Int, val height: Int) : Shape()
+    data class Circle(val radius: Int) : Shape()
+}
 
 fun main(){
 
