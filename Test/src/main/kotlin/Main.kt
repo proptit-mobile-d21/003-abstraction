108f877081c412f97e343ea87097b67b4e51e0a2
TrisTaam <yadartcon@gmail.com>
2023-05-26 20:48:41 +0700

    [EDIT] add Main.kt

---
+++ Test/src/main/kotlin/Main.kt
@@ -0,0 +1,52 @@
+interface UsableElectricity {
+    val isUseElectricity: Boolean
+}
+
+interface Function {
+    val purpose: String
+}
+
+abstract class Item(price: Int) : Function, UsableElectricity {
+    abstract fun showPrice()
+}
+
+interface Furniture : UsableElectricity {
+    override val isUseElectricity: Boolean
+        get() = false
+}
+
+interface Appliance : UsableElectricity {
+    override val isUseElectricity: Boolean
+        get() = true
+}
+
+class Sofa(private val price: Int) : Item(price), Furniture {
+    override fun showPrice() {
+        println("A sofa priced at $price");
+    }
+
+    override val purpose: String
+        get() = "To sit down and relax"
+}
+
+class Refrigerator(private val price: Int) : Item(price), Appliance {
+    override fun showPrice() {
+        println("A Refrigerator priced at $price")
+    }
+
+    override val purpose: String
+        get() = "To preserve food"
+}
+
+fun main(args: Array<String>) {
+    val item = listOf<Item>(
+        Sofa(10000), Refrigerator(7500), Sofa(15000), Refrigerator(12000)
+    )
+
+    item.forEach { x ->
+        x.showPrice()
+        println(x.purpose)
+        println("Is consuming electricity = ${x.isUseElectricity}")
+        println()
+    }
+}
