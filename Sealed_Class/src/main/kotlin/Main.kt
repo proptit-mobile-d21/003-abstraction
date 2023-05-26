b53fd150f61d1e62d412a7314e4b580e4838383d
mdunggggg <dungmanh74@gmail.com>
2023-05-26 23:22:36 +0700

    Tac dung Sealed Class

--- Sealed_Class/src/main/kotlin/Main.kt
+++ Sealed_Class/src/main/kotlin/Main.kt
@@ -30,4 +30,17 @@ fun main(){
     * VD: các enum constance của EnumShape bắt buộc phải có thuộc tính string còn các enum constance của Shape thì không.
     * Các enum constant chỉ có thể nằm trong body của enum class còn của sealed class thì có thể nằm ở ngoài body của sealed class.
     * */
+
+    /* Tác dụng của Sealed class
+    *       - Sealed Class cung cấp cho ta 1 hệ thống phân cấp các Class.
+    *       - Khi sử dụng hàm với When, khi không thể bao hàm hết các trường hợp thì ta dùng else, điều này
+    *       đôi lúc sẽ khiến kết quả chạy sai so với mong muốn, còn với when + sealed class khi không có đủ trường hợp
+    *       thì trình biên dịch sẽ báo lỗi để ta tìm đầy đủ các trường hợp
+    *       - Khi phát triển 1 cái gì đấy, trong đó đã có đầy đủ các chức năng cần thiết rồi và không muốn class đó được
+    *       kế thừa,.. thì dùng sealed class
+    *
+    *
+    *
+    *
+    * */
 }
