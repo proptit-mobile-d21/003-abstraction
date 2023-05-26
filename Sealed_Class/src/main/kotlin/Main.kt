48249e912d20642160ca563fd9afab54f915fb04
mdunggggg <dungmanh74@gmail.com>
2023-05-26 16:31:03 +0700

    Create a function named area to calculate the area of shapes, with the parameter shape

--- Sealed_Class/src/main/kotlin/Main.kt
+++ Sealed_Class/src/main/kotlin/Main.kt
@@ -1,9 +1,19 @@
+import kotlin.math.PI
+
 sealed class Shape{
     object None : Shape()
     data class Rectangle(val width: Int, val height: Int) : Shape()
     data class Circle(val radius: Int) : Shape()
 }
 
+fun area(shape : Shape) = when(shape){
+    is Shape.None -> 0
+    is Shape.Circle -> shape.radius * shape.radius * PI
+    is Shape.Rectangle -> shape.height * shape.width
+}
 fun main(){
-
+    val circle = Shape.Circle(5);
+    println(area(circle));
+    val rectangle = Shape.Rectangle(3, 4)
+    println(area(rectangle))
 }
