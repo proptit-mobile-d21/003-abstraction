a07cbb0a0efba04efd7dd813a9331c2010cf853c
mdunggggg <dungmanh74@gmail.com>
2023-05-31 16:34:45 +0700

    Update

--- BTVN/src/main/kotlin/Main.kt
+++ BTVN/src/main/kotlin/Main.kt
@@ -34,12 +34,19 @@ abstract class Register(private val mailService: IMailService, private val domai
 }
 
 class MessengerRegister(private val mailService: IMailService, domainServer: String) : Register(mailService, domainServer) {
+    private val listPerson = listOf<Person>(
+        Person("Dung1", "dungmanh1@gmail.com"),
+        Person("Dung2", "dungmanh2@gmail.com"),
+        Person("Dung3", "dungmanh2@gmail.com")
+    )
     override fun getDataUser(): Person {
-        return Person("Dung", "dungmanh74@gmail.com")
+        val i = (0..2).random()
+        return listPerson[i]
     }
 }
 
 fun main() {
+
     val sendGridMailService = SendGridMailService()
     val smtpMailService = SmtpMailService()
 
