55678b5280ca3ad84ec206b9367a96f74e7e5523
ProJulius <toanquen1314@gmail.com>
2023-05-31 11:43:21 +0700

    TrungLQ

--- test4/src/main/kotlin/Main.kt
+++ test4/src/main/kotlin/Main.kt
@@ -1,28 +1,106 @@
-abstract class Animal {
-    abstract fun sound()
-    fun sleep() {
-        println("The animal is sleeping")
+interface MailService {
+    fun mailService()
+}
+
+class SendMail: MailService{
+    override fun mailService() {
+        println("Send Mail")
     }
 }
 
-class Dog : Animal() {
-    override fun sound() {
-        println("Woof woof!")
+class ReceiveMail: MailService{
+    override fun mailService() {
+        println("Receive Mail")
     }
 }
 
-class Cat : Animal() {
-    override fun sound() {
-        println("Meow!")
+interface OTPService {
+    fun otpService()
+}
+
+class SendOTP: OTPService{
+    override fun otpService() {
+        println("Send OTP")
+    }
+}
+
+class ResendOTP: OTPService{
+    override fun otpService() {
+        println("Resend OTP")
+    }
+}
+
+interface Confirm{
+    fun confirmInput()
+}
+abstract class Register: MailService, Confirm{
+    abstract val userName: String
+    abstract val userData: String
+
+    abstract fun show()
+}
+
+class MessengerRegister : Register() {
+    override val userName: String = "Name Messenger: Trung LQ"
+    override val userData: String
+        get() = userName + " Password: 0333982632"
+
+    override fun show() {
+        println(userData)
+    }
+
+    override fun mailService() {
+        //val choose = SendMail()
+        val choose = ReceiveMail()
+        choose.mailService()
+    }
+
+    override fun confirmInput() {
+        println("Confirmed Messenger with $userName")
+    }
+}
+
+class FacebookRegister : Register(), OTPService {
+    override val userName: String = "Name Facebook: Trung"
+    override val userData: String
+        get() = userName + " Password: lt1412003"
+
+    override fun show() {
+        println(userData)
+    }
+
+    override fun mailService() {
+        val choose = SendMail()
+        //val choose = ReceiveMail()
+        choose.mailService()
+    }
+
+    override fun confirmInput() {
+        println("Confirmed Facebook with $userName")
+    }
+
+    override fun otpService() {
+        //val choose = SendOTP()
+        val choose = ResendOTP()
+        choose.otpService()
     }
 }
 
 fun main() {
-    val dog = Dog()
-    dog.sound()
-    dog.sleep()
+    println("Messenger: ")
+    val messengerRegister = MessengerRegister()
+    messengerRegister.mailService()
+    messengerRegister.confirmInput()
+    messengerRegister.show()
 
-    val cat = Cat()
-    cat.sound()
-    cat.sleep()
+    println("\nFacebook: ")
+    val facebookRegister = FacebookRegister()
+    facebookRegister.mailService()
+    facebookRegister.otpService()
+    facebookRegister.confirmInput()
+    facebookRegister.show()
+
 }
+
+
+
