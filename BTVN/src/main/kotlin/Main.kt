4bdb53b29da65d87e5d276f0eefdc03eb1590e40
mdunggggg <dungmanh74@gmail.com>
2023-05-31 15:51:19 +0700

    Update

--- BTVN/src/main/kotlin/Main.kt
+++ BTVN/src/main/kotlin/Main.kt
@@ -1,21 +1,21 @@
 data class Person(val name: String, val mail: String)
 
 interface IMailService {
-    fun sendMail(person: Person, message: String)
+    fun sendMail(person: Person, message: String, domainServer: String)
 }
 
 class SendGridMailService : IMailService {
-    override fun sendMail(person: Person, message: String) {
+    override fun sendMail(person: Person, message: String, domainServer: String) {
         println("Connecting to SendGrid server...")
-        println("Connecting successfully")
+        println("Connecting successfully to $domainServer")
         println("Sending mail to ${person.mail} with message: $message")
     }
 }
 
 class SmtpMailService : IMailService {
-    override fun sendMail(person: Person, message: String) {
+    override fun sendMail(person: Person, message: String, domainServer: String) {
         println("Connecting to SMTP server...")
-        println("Connecting successfully")
+        println("Connecting successfully to $domainServer")
         println("Sending mail to ${person.mail} with message: $message")
     }
 }
@@ -23,13 +23,13 @@ class SmtpMailService : IMailService {
 abstract class Register(private val mailService: IMailService, private val domainServer: String) : IMailService {
     abstract fun getDataUser(): Person
 
-    override fun sendMail(person: Person, message: String) {
-        mailService.sendMail(person, message)
+    override fun sendMail(person: Person, message: String, domainServer: String) {
+        mailService.sendMail(person, message, domainServer)
     }
 
     fun submit() {
         val person = getDataUser()
-        sendMail(person, "Hello ${person.name}")
+        sendMail(person, "Hello ${person.name}", domainServer)
     }
 }
 
@@ -43,9 +43,9 @@ fun main() {
     val sendGridMailService = SendGridMailService()
     val smtpMailService = SmtpMailService()
 
-    val messengerRegister1 = MessengerRegister(sendGridMailService, "example.com")
+    val messengerRegister1 = MessengerRegister(sendGridMailService, "Sever1")
     messengerRegister1.submit()
 
-    val messengerRegister2 = MessengerRegister(smtpMailService, "example.com")
+    val messengerRegister2 = MessengerRegister(smtpMailService, "Sever2")
     messengerRegister2.submit()
 }
